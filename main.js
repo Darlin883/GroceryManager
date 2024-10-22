98d66905ded363906b3967bdeab3d2c1a3c85fb4
Jacob Rushinski <dsrush2@verizon.net>
2024-10-22 10:32:43 -0400

    Broken code

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
+document.addEventListener("click", function() { clickCheck(); });
+
+function clickCheck() {
+  let ele = document.getElementById("item");
+  let textValue = ele.value;
+  let newEle = document.getElementById(textValue);
+
+  if (newEle === null) {
+    newEle = "deafult";
+  } 
+    let tagName = null;
+    tagName = newEle.tagName;
+    console.log(tagName);
+
+  if (tagName === 'P') {
+    strikethrough(textValue);
+  } else if (tagName === 'S') {
+    unStrikethrough(textValue);
+  }
+}
+// document.getElementById(textValue).addEventListener("click", function() { strikethrough(textValue); });
+// document.getElementById(id).addEventListener("click", function() { unStrikethrough(id); });
+
 function addItem() {
   let ele = document.getElementById("item");
-
   let textValue = ele.value;
 
   if (textValue === "") { // Come back to, fix if you put a space itll add it to the list
@@ -12,13 +34,23 @@ function addItem() {
     fList.textContent = textValue; 
     fList.classList.add("full-item-list"); // Adds the CSS element to the tag
     loc.appendChild(fList);
-    
-    document.getElementById(textValue).addEventListener("click", function() { strikethrough(textValue); });
   }
 }
 
 function strikethrough(id) {
   let newElement = document.createElement('s');
+  newElement.setAttribute("id", id)
+  newElement.textContent = id;
+  newElement.classList.add("fList-strike");
+  let oldElement = document.getElementById(id);
+  oldElement.replaceWith(newElement);
+}
+
+function unStrikethrough(id) {
+  let newElement = document.createElement('p');
+  newElement.setAttribute("id", id)
+  newElement.textContent = id;
+  newElement.classList.add("fList-strike");
   let oldElement = document.getElementById(id);
   oldElement.replaceWith(newElement);
 }
