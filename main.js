5cedefa5a9095ed365e4a84fc2222162a426f381
Jacob Rushinski <dsrush2@verizon.net>
2024-10-22 08:01:55 -0400

    append full list v1

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ function addItem() {
   let ele = document.getElementById("item");
 
   let textValue = ele.value;
-  console.log("New value:", textValue);
+
+  if (textValue === "") { // Come back to, fix if you put a space itll add it to the list
+    console.log("test");
+  } else {
+    let loc = document.getElementById("item-list-id"); 
+    let fList = document.createElement("li");
+    fList.textContent = textValue; 
+    fList.classList.add("full-item-list"); // Adds the CSS element to the tag
+    loc.appendChild(fList); // Puts message onto screen
+  }
 }
   
