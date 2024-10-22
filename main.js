0c556fca7f7312e6ec5dbd290c3eb0a930f15138
Jacob Rushinski <dsrush2@verizon.net>
2024-10-22 08:47:43 -0400

    halfway strikethrough commit

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ function addItem() {
     console.log("test");
   } else {
     let loc = document.getElementById("item-list-id"); 
-    let fList = document.createElement("li");
+    let fList = document.createElement("p");
+    fList.setAttribute("id", textValue)
     fList.textContent = textValue; 
     fList.classList.add("full-item-list"); // Adds the CSS element to the tag
-    loc.appendChild(fList); // Puts message onto screen
+    loc.appendChild(fList);
+    
+    document.getElementById(textValue).addEventListener("click", function() { strikethrough(textValue); });
   }
+}
+
+function strikethrough(id) {
+  let newElement = document.createElement('s');
+  let oldElement = document.getElementById(id);
+  oldElement.replaceWith(newElement);
 }
   
